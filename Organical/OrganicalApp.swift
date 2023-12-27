9296213b0d6bb97c554860d880156884404edab1
Khya Ali <khyaali@protonmail.com>
2023-12-27 02:43:53 +0000

    Initial Commit

---
+++ Organical/OrganicalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganicalApp.swift
+//  Organical
+//
+//  Created by Khya Ali on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct OrganicalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
